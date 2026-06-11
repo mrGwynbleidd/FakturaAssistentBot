312166563a28acf70a0ba2aa4556b54329dcfd53
mrGwynbleidd <axmedovshakh@gmail.com>
2026-06-11 10:44:57 +0500

    Add

--- build_index.py
+++ build_index.py
@@ -1,7 +1,5 @@
 #call function from index_builder
-from sentence_transformers import SentenceTransformer
-model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
-model.save("./models/embedding_model")
+
 
 from app.rag.index_builder import build_index
 
